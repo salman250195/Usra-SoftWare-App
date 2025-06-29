16fe6ccf473f752235e9ad290ccb817855a53452
Salman Khan <salman250195@gmail.com>
2025-06-29 15:22:14 +0530

    Updated new features

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,8 +15,8 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, { useHash: true,
-    // scrollPositionRestoration: 'enabled',
-    // anchorScrolling: 'enabled'
+    scrollPositionRestoration: 'enabled',
+    anchorScrolling: 'enabled'
    },)],
   exports: [RouterModule]
 })
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private titleService: Title, private metaService: Meta) { }
 
   ngOnInit(): void {
+    this.titleService.setTitle('Usra Software | Ethical Software Development in India');
+    this.metaService.updateTag({
+      name: 'description',
+    content: 'Usra Software offers ethical and innovative IT solutions in web development, mobile apps, UI/UX design, digital marketing, and more — with values rooted in faith and excellence.'
+    });
+    this.metaService.updateTag({
+      name: 'keywords',
+    content: 'Usra Software, software development India, ethical tech, web development, mobile app, UI UX design, digital marketing'
+    })
   }
 
 }
